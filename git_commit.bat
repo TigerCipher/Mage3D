b5ca0e71bca8e76710187b326b22d502395f7343
tigercipher <matt.rogers8396@gmail.com>
2020-07-28 00:53:23 -0500

    quick change to git batch

--- git_commit.bat
+++ git_commit.bat
@@ -1,7 +1,9 @@
 @echo off
 echo Adding all non ignored files
 git add -A
+echo Checking status
+git status
+
 set /p msg="Enter commit message: "
 echo Committing added files
 git commit -m "%msg%"
-pause
--- git_commit_push.bat
+++ git_commit_push.bat
@@ -1,8 +1,6 @@
 @echo off
 echo Staging untracked, modified, and deleted files
 CALL git_commit.bat
-echo Checking status
-git status
 echo Pushing committed files with message: "%msg%"
 CALL git_push.bat
 echo Process finished
