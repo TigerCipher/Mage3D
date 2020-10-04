a1141d09fde3eb9333f0109f69c4e81b5f874748
tigercipher <matt.rogers8396@gmail.com>
2020-10-04 00:50:51 -0500

    Fixed license header

--- Mage3DX/Vertex.h
+++ Mage3DX/Vertex.h
@@ -1,11 +1,30 @@
+/*
+ * Mage3DX
+ * Copyright (C) 2020 Blue Moon Development. All rights reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contact: team@bluemoondev.org
+ * 
+ * File Name: Vertex.h
+ * Date File Created: 10/2/2020 at 8:28 PM
+ * Author: Matt
+ */
+
 #pragma once
 
 #include "MathHelper.h"
-#include "Log.h"
 
 #include "Graphics.h"
 
-#include <bmd/types.h>
 
 
 namespace mage
